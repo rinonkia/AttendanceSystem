01923b5338c7247301afbad2a401e146fe70cd41
gatapon <git@gatapon.net>
2018-12-29 11:36:07 +0900

    TimestampController and Route create

---
+++ app/Http/Controllers/TimestampsController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+use Carbon\Carbon;
+use App\User;
+use App\Timestamp;
+
+class TimestampsController extends Controller
+{
+    
+}
