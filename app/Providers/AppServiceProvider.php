58c712c34ddd06e6173f65cc9d081f66a52cd9df
gatapon <git@gatapon.net>
2018-12-31 16:05:25 +0900

    add procfile

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,6 +14,10 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         \Schema::defaultStringLength(191);
+
+        if(\App::environment('production')) {
+            \URL::forceScheme('https');
+        }
     }
 
     /**
