ad2cae772fc14bdf8739d250867e34a966c4a7c4
gatapon <git@gatapon.net>
2019-01-06 18:27:03 +0900

    fix

--- app/Http/Controllers/TimestampsController.php
+++ app/Http/Controllers/TimestampsController.php
@@ -16,29 +16,26 @@ class TimestampsController extends Controller
         $user = Auth::user();
 
         /**
-         * 打刻は1日一回までにしたい  保留
+         * 打刻は1日一回までにしたい  
          */
         $oldTimestamp = Timestamp::where('user_id', $user->id)->latest()->first();
         if ($oldTimestamp) {
             $oldTimestampDay = $this->timestampParseDay($oldTimestamp->punchIn);
         }
 
-        //dd($oldTimestampDay);
-
         $todayTimestamp = Carbon::now();
         $newTimestampDay = $this->timestampParseDay($todayTimestamp);
-        //dd($newTimestampDay);
 
+        /**
+         * 日付を比較する。同日付の出勤打刻で、かつ直前のTimestampの退勤打刻がされていない場合エラーを吐き出す。
+         */
         if (($oldTimestampDay == $newTimestampDay) && (empty($oldTimestamp->punchOut))){
             return redirect()->back()->with('error', 'すでに出勤打刻がされています');
         }
-        // if ($oldTimestampDay[0] === $today[0]) {
-        //    return redirect()->back()->with('error', '既に打刻されているか、もしくは働きすぎです');
-        //}
 
         $timestamp = Timestamp::create([
             'user_id' => $user->id,
-            'punchIn' => Carbon::now()
+            'punchIn' => $todayTimestamp,
         ]);
 
         return redirect()->back()->with('my_status', '出勤打刻が完了しました');
@@ -59,14 +56,12 @@ class TimestampsController extends Controller
         return redirect()->back()->with('my_status', '退勤打刻が完了しました');
     }
     /**
-     * Y-m-d H:m:s から　Y-m-dを取り出す
+     * Y-m-d H:m:s から　Y-m-dだけを取り出す
      */
     public function timestampParseDay($timestamp)
     {
         $parseTimestamp[] = explode(' ', $timestamp);
         $parseDay = $parseTimestamp[0][0];
-
-        //dd($parseTimestamp);
 
         return $parseDay;
     }
